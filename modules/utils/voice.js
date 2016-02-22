b1cf9de3b7d989e244a2f86394308b5e9dd91e78
Morten Olsen <morten@olsen.io>
2016-02-22 11:46:09 +0100

    fix voice

--- modules/utils/voice.js
+++ modules/utils/voice.js
@@ -3,38 +3,26 @@ boot.def('utils/voice', [], function (exports) {
 
 	me.read = function (message, cb) {
 		var msg = new SpeechSynthesisUtterance();
-		var voices = window.speechSynthesis.getVoices();
-		msg.voiceURI = 'native';
-		msg.volume = 1; // 0 to 1
-		msg.rate = 1; // 0.1 to 10
-		msg.pitch = 1; //0 to 2
+		var voices = window.speechSynthesis.getVoices().filter(v => v.lang === 'en-GB');
+		msg.voice = voices[0];
 		msg.text = message.replace(/\./g, ' .stop. ');
-		msg.lang = 'en-UK';
 
 		msg.onend = function(e) {
 			if (!!cb) cb();
 		};
-
-		msg.voice = voices[1]; // Note: some voices don't support altering params
 		console.log(msg);
 		speechSynthesis.speak(msg);
 	}
 
 	me.say = function (message, cb) {
 		var msg = new SpeechSynthesisUtterance();
-		var voices = window.speechSynthesis.getVoices();
-		msg.voiceURI = 'native';
-		msg.volume = 1; // 0 to 1
-		msg.rate = 1; // 0.1 to 10
-		msg.pitch = 1; //0 to 2
+		var voices = window.speechSynthesis.getVoices().filter(v => v.lang === 'en-GB');
+		msg.voice = voices[1];
 		msg.text = message;
-		msg.lang = 'en-UK';
 
 		msg.onend = function(e) {
 			if (!!cb) cb();
 		};
-
-		msg.voice = voices[2]; // Note: some voices don't support altering params
 		console.log(msg);
 		speechSynthesis.speak(msg);
 	}
